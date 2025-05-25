a574ae49b4c0d936e96ff259596bc6b230f3986d
Cristhian <cristhian.bravo.8657@gmail.com>
2025-05-24 23:27:25 -0500

    Se creo el proyecto tienda01, y se crearon los modelos producto y venta

--- tienda/productos/models.py
+++ tienda/productos/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
-
-    def __str__(self):
-        return self.nombre
+	codigo = models.CharField(max_length=100, unique=True)
+	nombre = models.CharField(max_length=200)
+	descripcion = models.TextField()
+	precio = models.DecimalField(max_digits=10, decimal_places=2)
+ 
+	def __str__(self):
+		return self.nombre
