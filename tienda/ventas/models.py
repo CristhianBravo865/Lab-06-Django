d851079328274ae1ebfde166c2fa0ee7db4f1b6f
Cristhian <cristhian.bravo.8657@gmail.com>
2025-05-24 22:40:33 -0500

    Crear modelos Producto, Venta y DetalleVenta

--- tienda/ventas/models.py
+++ tienda/ventas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from productos.models import Producto
 
-# Create your models here.
+class Venta(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    productos = models.ManyToManyField(Producto, through='DetalleVenta')
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+
+    def subtotal(self):
+        return self.producto.precio * self.cantidad
